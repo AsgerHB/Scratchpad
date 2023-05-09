7d69789a293d6b900fdd3010528b3bbf3d90f8b3
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-05-09 16:20:06 +0200

    aaa

--- run_slurm.sh
+++ run_slurm.sh
@@ -3,4 +3,10 @@
 export UPPAAL_DIR=OilPump/uppaal-4.1.20-stratego-10-linux64/
 export WORkING_DIR=OPEvaluations/
 export EXECUTOR="sbatch --out=/dev/null --partition=rome -n1 --mem=1G "
-./run_experiment.sh
+$EXECUTOR script1.sh
+$EXECUTOR script2.sh
+$EXECUTOR script3.sh
+$EXECUTOR script4.sh
+$EXECUTOR script5.sh
+$EXECUTOR script6.sh
+$EXECUTOR script7.sh
