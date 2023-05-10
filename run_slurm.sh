83350698ac6ad6f8690e369f61bc1052a3052e15
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-05-10 12:12:32 +0200

    yee

--- run_slurm.sh
+++ run_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export UPPAAL_DIR=~/OilPump/uppaal-4.1.20-stratego-10-linux64/
-export WORKING_DIR=~/OPEvaluations/
+export WORKING_DIR=Evaluations/
 export EXECUTOR="sbatch --out=/dev/null --partition=rome -n1 --mem=1G "
 $EXECUTOR script1.sh
 $EXECUTOR script2.sh
--- script3.sh
+++ script3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir $WORKING_DIR'/OP 1 Samples 0.5 G/out'
+mkdir -p $WORKING_DIR'/OP 1 Samples 0.5 G/out' -f
 /usr/bin/time -v $UPPAAL_DIR/bin/verifyta.sh $WORKING_DIR'/OP 1 Samples 0.5 G/OP__PreShielded.xml'$WORKING_DIR'/OP 1 Samples 0.5 G/TrainSaveCheckSafety.q' --silence-progress --max-iterations 1 --good-runs 12000 --total-runs 12000 --runs-pr-state 12000 --epsilon 0.000001 --alpha 0.01 --beta 0.01 2>&1 > $WORKING_DIR'/OP 1 Samples 0.5 G/out'
